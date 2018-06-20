32c8cf03e388e30aec05b4e89d3d12b0802de4b3
Ross Zurowski <ross@rosszurowski.com>
2018-06-20 16:53:23 -0700

    Add basic photo cycling

--- src/components/service-list.js
+++ src/components/service-list.js
@@ -9,51 +9,59 @@ const services = [
     title: 'Enrollment',
     description:
       'Easily enroll members, collection personal information and biometrics, and issue membership cards.',
+    imageUrl: 'https://placekitten.com/900/550',
   },
   {
     icon: 'identification',
     title: 'Identification',
     description:
       'Accurately identify members, edit their personal information, and confirm their eligibility for services.',
+    imageUrl: 'https://placekitten.com/g/900/600',
   },
   {
     icon: 'submission',
     title: 'Claims submission',
     description:
       'Efficiently collect claims and any corroborating documents from clinics, pharmacies, and hospitals.',
+    imageUrl: 'https://placekitten.com/900/565',
   },
   {
     icon: 'processing',
     title: 'Claims processing',
     description:
       'Thoroughly review and adjudicate claims, and manage the provider reimbursement process.',
+    imageUrl: 'https://placekitten.com/g/900/700',
   },
   {
     icon: 'reporting',
     title: 'Reporting',
     description:
       'Intelligently analyze data across the entire system and make any necessary changes.',
+    imageUrl: 'https://placekitten.com/900/800',
   },
 ];
 
 export default class ServicesList extends Component {
   state = {
-    activeService: services[0].title,
+    activeServiceTitle: services[0].title,
   };
 
   render() {
-    const { activeService } = this.state;
+    const { activeServiceTitle } = this.state;
+    const activeService = services.find(service => service.title === activeServiceTitle);
 
     return (
       <div className="flex-m gutter-adjust">
         <ul className="column-5-m gutter">
           {services.map((service, index) => (
-            <li className={classNames({ 'marginTop-2': index !== 0 })} key={service.title}>
+            <li
+              className={classNames({ 'marginTop-2': index !== 0 })}
+              key={service.title}>
               <ServiceCard
                 title={service.title}
                 iconName={service.icon}
-                active={activeService === service.title}
-                onClick={() => this.setState({ activeService: service.title })}>
+                active={activeServiceTitle === service.title}
+                onClick={() => this.setState({ activeServiceTitle: service.title })}>
                 {service.description}
               </ServiceCard>
             </li>
@@ -62,7 +70,8 @@ export default class ServicesList extends Component {
         <div className="display-none display-block-m column-7 gutter">
           <img
             className="borderRadius-4"
-            src="https://placekitten.com/900/550"
+            src={activeService.imageUrl}
+            alt={activeService.title}
           />
         </div>
       </div>
