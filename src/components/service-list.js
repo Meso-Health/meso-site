dcf9f17cbeabccb51f4cc0c2d9df14a30c45593e
Ross Zurowski <ross@rosszurowski.com>
2018-06-21 16:04:55 -0700

    Add re-positioning for How it Works section

--- src/components/service-list.js
+++ src/components/service-list.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import throttle from 'lodash.throttle';
 
 import ServiceCard from './service-card';
+
+import enrollmentImageUrl from '../assets/img/service-enrollment@2x.jpg';
+import identificationImageUrl from '../assets/img/service-identification@2x.jpg';
+import submissionImageUrl from '../assets/img/service-submission@2x.jpg';
+import processingImageUrl from '../assets/img/service-processing@2x.jpg';
+import reportingImageUrl from '../assets/img/service-reporting@2x.jpg';
 
 const services = [
   {
@@ -9,67 +16,130 @@ const services = [
     title: 'Enrollment',
     description:
       'Easily enroll members, collection personal information and biometrics, and issue membership cards.',
-    imageUrl: 'https://placekitten.com/900/550',
+    imageUrl: enrollmentImageUrl,
   },
   {
     icon: 'identification',
     title: 'Identification',
     description:
       'Accurately identify members, edit their personal information, and confirm their eligibility for services.',
-    imageUrl: 'https://placekitten.com/g/900/600',
+    imageUrl: identificationImageUrl,
   },
   {
     icon: 'submission',
     title: 'Claims submission',
     description:
       'Efficiently collect claims and any corroborating documents from clinics, pharmacies, and hospitals.',
-    imageUrl: 'https://placekitten.com/900/565',
+    imageUrl: submissionImageUrl,
   },
   {
     icon: 'processing',
     title: 'Claims processing',
     description:
       'Thoroughly review and adjudicate claims, and manage the provider reimbursement process.',
-    imageUrl: 'https://placekitten.com/g/900/700',
+    imageUrl: processingImageUrl,
   },
   {
     icon: 'reporting',
     title: 'Reporting',
     description:
       'Intelligently analyze data across the entire system and make any necessary changes.',
-    imageUrl: 'https://placekitten.com/900/800',
+    imageUrl: reportingImageUrl,
   },
 ];
 
+const range = (val, min, max) => Math.min(Math.max(min, val), max);
+
 export default class ServicesList extends Component {
   state = {
-    activeServiceTitle: services[0].title,
+    activeServiceIndex: 0,
+    cardTops: [],
+    maxOffset: 0,
+    assumedImageSizeMeasured: false,
   };
 
+  parentRef = React.createRef();
+  photoRef = React.createRef();
+  cardRefs = services.map(() => React.createRef());
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    requestAnimationFrame(() => {
+      this.handleResize();
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    this.throttledMeasure();
+  }
+
+  handleImageLoad = () => {
+    this.measure();
+    this.setState({ assumedImageSizeMeasured: true });
+  }
+
+  measure = () => {
+    if (this.parentRef.current && this.photoRef.current) {
+      const parentRect = this.parentRef.current.getBoundingClientRect();
+      const parentHeight = parentRect.height;
+      const photoHeight = this.photoRef.current.getBoundingClientRect().height;
+      const cardTops = services.map((_, index) => {
+        const cardRect = this.cardRefs[index].current.getBoundingClientRect();
+
+        return Math.abs(parentRect.top - cardRect.top);
+      });
+
+      const maxOffset = parentHeight - photoHeight;
+
+      this.setState({ cardTops, maxOffset });
+    }
+  }
+
+  throttledMeasure = throttle(this.measure, 100);
+
   render() {
-    const { activeServiceTitle } = this.state;
-    const activeService = services.find(service => service.title === activeServiceTitle);
+    const { activeServiceIndex, cardTops, maxOffset } = this.state;
+    const activeService = services[activeServiceIndex];
+
+    const top = cardTops[activeServiceIndex];
+    const offset = range(top, 0, maxOffset);
 
     return (
-      <div className="flex-m gutter-adjust">
+      <div className="ServiceList position-relative flex-m gutter-adjust" ref={this.parentRef}>
         <ul className="column-5-m gutter">
           {services.map((service, index) => (
             <li
-              className={classNames({ 'marginTop-2': index !== 0 })}
+              className={classNames({ 'paddingBottom-2': index !== services.length - 1 })}
+              ref={this.cardRefs[index]}
               key={service.title}>
               <ServiceCard
                 title={service.title}
                 iconName={service.icon}
-                active={activeServiceTitle === service.title}
-                onClick={() => this.setState({ activeServiceTitle: service.title })}>
+                active={activeServiceIndex === index}
+                onClick={() => this.setState({ activeServiceIndex: index })}>
                 {service.description}
               </ServiceCard>
+              <img
+                className={classNames('display-none-m borderRadius-4 borderTopRadius-0', {
+                  'position-absolute visibility-hidden': activeServiceIndex !== index
+                })}
+                src={service.imageUrl}
+                alt={activeService.title} />
             </li>
           ))}
         </ul>
         <div className="display-none display-block-m column-7 gutter">
           <img
-            className="borderRadius-4"
+            className="ServiceList-photo position-relative borderRadius-4"
+            ref={this.photoRef}
+            onLoad={this.state.assumedImageSizeMeasured ? undefined : this.handleImageLoad}
+            style={{ transform: `translateY(${offset}px)` }}
+            width="1200"
+            height="951"
             src={activeService.imageUrl}
             alt={activeService.title}
           />
