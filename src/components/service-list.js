a4c959e3bc18d51afb3e61ca144540be528fece4
Ross Zurowski <ross@rosszurowski.com>
2018-06-22 17:17:04 -0700

    Add responsive image approach

--- src/components/service-list.js
+++ src/components/service-list.js
@@ -5,49 +5,56 @@ import throttle from 'lodash.throttle';
 import utils from '../utils';
 import ServiceCard from './service-card';
 
-import enrollmentImageUrl from '../assets/img/service-enrollment@2x.jpg';
-import identificationImageUrl from '../assets/img/service-identification@2x.jpg';
-import submissionImageUrl from '../assets/img/service-submission@2x.jpg';
-import processingImageUrl from '../assets/img/service-processing@2x.jpg';
-import reportingImageUrl from '../assets/img/service-reporting@2x.jpg';
-
 const services = [
   {
     icon: 'enrollment',
     title: 'Enrollment',
     description:
       'Easily enroll members, collection personal information and biometrics, and issue membership cards.',
-    imageUrl: enrollmentImageUrl,
+    normalImageUrl: require('../assets/img/service-enrollment@1x.jpg'),
+    retinaImageUrl: require('../assets/img/service-enrollment@2x.jpg'),
   },
   {
     icon: 'identification',
     title: 'Identification',
     description:
       'Accurately identify members, edit their personal information, and confirm their eligibility for services.',
-    imageUrl: identificationImageUrl,
+    normalImageUrl: require('../assets/img/service-identification@1x.jpg'),
+    retinaImageUrl: require('../assets/img/service-identification@2x.jpg'),
   },
   {
     icon: 'submission',
     title: 'Claims submission',
     description:
       'Efficiently collect claims and any corroborating documents from clinics, pharmacies, and hospitals.',
-    imageUrl: submissionImageUrl,
+    normalImageUrl: require('../assets/img/service-submission@1x.jpg'),
+    retinaImageUrl: require('../assets/img/service-submission@2x.jpg'),
   },
   {
     icon: 'processing',
     title: 'Claims processing',
     description:
       'Thoroughly review and adjudicate claims, and manage the provider reimbursement process.',
-    imageUrl: processingImageUrl,
+    normalImageUrl: require('../assets/img/service-processing@1x.jpg'),
+    retinaImageUrl: require('../assets/img/service-processing@2x.jpg'),
   },
   {
     icon: 'reporting',
     title: 'Reporting',
     description:
       'Intelligently analyze data across the entire system and make any necessary changes.',
-    imageUrl: reportingImageUrl,
+    normalImageUrl: require('../assets/img/service-reporting@1x.jpg'),
+    retinaImageUrl: require('../assets/img/service-reporting@2x.jpg'),
   },
 ];
+
+const getImageProps = service => ({
+  src: service.normalImageUrl,
+  srcSet: `${service.normalImageUrl}, ${service.retinaImageUrl} 2x`,
+  alt: service.title,
+  width: 1200,
+  height: 951,
+});
 
 export default class ServicesList extends Component {
   state = {
@@ -146,8 +153,7 @@ export default class ServicesList extends Component {
                       activeServiceIndex !== index,
                   },
                 )}
-                src={service.imageUrl}
-                alt={activeService.title}
+                {...getImageProps(service)}
               />
             </li>
           ))}
@@ -162,10 +168,7 @@ export default class ServicesList extends Component {
                 : this.handleImageLoad
             }
             style={{ transform: `translateY(${offset}px)` }}
-            width="1200"
-            height="951"
-            src={activeService.imageUrl}
-            alt={activeService.title}
+            {...getImageProps(activeService)}
           />
         </div>
       </div>
