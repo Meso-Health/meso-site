c0c510afe05ec48483a1204fcf984a5f043039c8
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2020-01-30 14:37:47 -0800

    Improve styling of line breaks and links within FAQ answers

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -5,6 +5,16 @@ import Menu from '../components/menu';
 import Section from '../components/section';
 import content from '../content.yml';
 
+function FaqItem ({ question, answer }) {
+  // note: there are custom styles applied to <p> and <a> elements within FaqItem specified in base.css
+  return (
+    <div className="FaqItem marginBottom-3">
+      <h3 className="fontWeight-bold marginVertical-2">{question}</h3>
+      <ReactMarkdown source={answer} linkTarget="_blank" />
+    </div>
+  );
+}
+
 export default function Faq() {
   return (
     <Section className="position-relative overflow-hidden paddingTop-3 paddingTop-4-m paddingBottom-4">
@@ -16,10 +26,7 @@ export default function Faq() {
           </h1>
         </div>
         {content.faqItems.map(item => (
-          <div className="marginBottom-3">
-            <h3 className="fontWeight-bold marginVertical-2">{item.question}</h3>
-            <ReactMarkdown source={item.answer} linkTarget="_blank" />
-          </div>
+          <FaqItem question={item.question} answer={item.answer} />
         ))}
       </div>
     </Section>
