420b9261797cb8862c1466de4b030a79f3f9421f
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2020-01-31 12:18:28 -0800

    Clean up product page layout

--- src/pages/product.js
+++ src/pages/product.js
@@ -6,16 +6,16 @@ import Section from '../components/section';
 export default function Product() {
   return (
     <Fragment>
-      <Section dark className="overflow-hidden paddingTop-3 paddingTop-4-m paddingBottom-4">
-        <Menu dark />
-      </Section>
-      <Section className="position-relative overflow-hidden paddingTop-3 paddingTop-4-m paddingBottom-4">
+    <Section dark className="paddingVertical-3">
+      <Menu dark />
+    </Section>
+      <Section className="paddingTop-3 paddingTop-4-m paddingBottom-4">
         <div className="container">
-          <h1 className="fontSize-36 fontSize-48-m fontWeight-light lineHeight-1d25 marginTop-4">
+          <h1 className="fontSize-36 fontSize-48-m fontWeight-light lineHeight-1d25">
             Product
           </h1>
         </div>
-        <div className="position-relative container">
+        <div className="container marginTop-4">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </Section>
